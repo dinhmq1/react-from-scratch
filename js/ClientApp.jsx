d1e0dd0d430ef65e4f83d23d9bb542986673d75e
mike <dinhmq1@gmail.com>
2018-01-25 03:42:03 -0600

    refactored ClientApp.jsx

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const ce = React.createElement;
-
 const MyTitle = function(props) {
-  return React.createElement('div', null, ce('h1', { style: { color: props.color } }, props.title));
-};
-
-const MyFirstComponent = function() {
-  return ce(
-    'div',
-    { id: 'my-first-component' },
-    ce(MyTitle, { title: 'Pineapple Express', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'Silicon Valley', color: 'GreenYellow' }),
-    ce(MyTitle, { title: 'Crashing', color: 'peru' }),
-    ce(MyTitle, { title: 'Seinfeld', color: 'Blue' })
+  // return React.createElement('div', null, ce('h1', { style: { color: props.color } }, props.title));
+  const style = {color: props.color}
+  return (
+    <div>
+      <h1 style={style}>{props.title}</h1>
+    </div>
   );
 };
 
-render(ce(MyFirstComponent), document.getElementById('app'));
+const MyFirstComponent = function() {
+  return(
+      <div id="my-first-component">
+        <MyTitle title="Game of Thrones" color="YellowGreen" />
+        <MyTitle title="Silicon Valley" color="Peru" />
+        <MyTitle title="Seinfeld" color="SteelBlue" />
+        <MyTitle title="Bob Ross" color="Turquoise" />
+      </div>
+    );
+};
+
+render(<MyFirstComponent />, document.getElementById('app'));
