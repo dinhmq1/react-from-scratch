b2a1359ca544a592edcd41799b7b9d5e422c6ca2
mike <dinhmq1@gmail.com>
2018-01-25 09:52:57 -0600

    update

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import preload from '../data.json';
+import React from "react";
+import preload from "../data.json";
 
 const Search = () => (
-    <div className='search'>
-        {preload.shows.map(show => <h1>{show.title}</h1>)}
-    </div>
+  <div className="search">
+    {preload.shows.map(show => (
+      <div className="show-card">
+        <img
+          alt={`${show.title} Show Poster`}
+          src={`/public/poster/${show.poster}`}
+        />
+        <div>
+          <h3>{show.title}</h3>
+          <h4>{show.year}</h4>
+          <p>{show.description}</p>
+        </div>
+      </div>
+    ))}
+  </div>
 );
 
 export default Search;
